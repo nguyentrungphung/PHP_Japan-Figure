e9ad0caa126ac22762f7bfa9662fa350101713fe
Cam Ba Mao <maocam55@gmail.com>
2020-06-10 11:46:08 +0700

    phan đau va phan cuoi trang web

---
+++ header-client.php
@@ -0,0 +1,377 @@
+<?php
+session_start();
+//tiến hành kiểm tra là người dùng đã đăng nhập hay chưa
+//nếu chưa, chuyển hướng người dùng ra lại trang đăng nhập
+if (!isset($_SESSION['username'])) {
+	//  header('Location: login.php');
+	echo "<h2>Bạn chưa đăng nhập <a href='/Figure_shop/login.php'>Login</a> -<a href='/Figure_shop/register.php'>Register</a></h2>";
+}
+else
+{
+	echo "<h2>Xin chào: ".$_SESSION['username']."<a href='/Figure_shop/logout.php'>Logout</a></h2>";
+}
+?>
+	
+
+
+	
+	<!-- // if(isset($_SESSION['user'])!="")
+	// {
+	// 	echo "<h2>Xin chào: ".$_SESSION['user']."<a href='/Figure_shop/logout.php'>Logout</a></h2>";
+	// }
+	// else{
+	// 	echo "<h2>Bạn chưa đăng nhập <a href='/Figure_shop/login.php'>Login</a> -<a href='/Figure_shop/register.php'>Register</a></h2>";
+	// } -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Smart Shop a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Smart Shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- pignose css -->
+<link href="css/pignose.layerslider.css" rel="stylesheet" type="text/css" media="all" />
+
+
+<!-- //pignose css -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- //js -->
+<!-- cart -->
+	<script src="js/simpleCart.min.js"></script>
+<!-- cart -->
+<!-- for bootstrap working -->
+	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+<!-- //for bootstrap working -->
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
+<script src="js/jquery.easing.min.js"></script>
+</head>
+<body>
+
+<!-- header -->
+<div class="header">
+	<div class="container">
+		<ul>
+			<li><span class="glyphicon glyphicon-time" aria-hidden="true"></span>Free and Fast Delivery</li>
+			<li><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>Free shipping On all orders</li>
+			<li><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><a href="mailto:info@example.com">info@example.com</a></li>
+		</ul>
+	</div>
+</div>
+<!-- //header -->
+<!-- header-bot -->
+<div class="header-bot">
+	<div class="container">
+		<div class="col-md-3 header-left">
+			<h1><a href="list_product.php"><img src="images/logo3.jpg"></a></h1>
+		</div>
+		<div class="col-md-6 header-middle">
+			<form>
+				<div class="search">
+					<input type="search" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}" required="">
+				</div>
+				<div class="section_room">
+					<select id="country" onchange="change_country(this.value)" class="frm-field required">
+						<option value="null">All categories</option>
+						<option value="null">Electronics</option>     
+						<option value="AX">kids Wear</option>
+						<option value="AX">Men's Wear</option>
+						<option value="AX">Women's Wear</option>
+						<option value="AX">Watches</option>
+					</select>
+				</div>
+				<div class="sear-sub">
+					<input type="submit" value=" ">
+				</div>
+				<div class="clearfix"></div>
+			</form>
+		</div>
+		<div class="col-md-3 header-right footer-bottom">
+			<ul>
+				<li><a href="#" class="use1" data-toggle="modal" data-target="#myModal4"><span>Login</span></a>
+					
+				</li>
+				<li><a class="fb" href="#"></a></li>
+				<li><a class="twi" href="#"></a></li>
+				<li><a class="insta" href="#"></a></li>
+				<li><a class="you" href="#"></a></li>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //header-bot -->
+<!-- banner -->
+<div class="ban-top">
+	<div class="container">
+		<div class="top_nav_left">
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse menu--shylock" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav menu__list">
+					<li class="active menu__item menu__item--current"><a class="menu__link" href="index.php">Home <span class="sr-only">(current)</span></a></li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">men's wear <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="row">
+									<div class="col-sm-6 multi-gd-img1 multi-gd-text ">
+										<a href="mens.html"><img src="images/woo1.jpg" alt=" "/></a>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Clothing</a></li>
+											<li><a href="mens.html">Wallets</a></li>
+											<li><a href="mens.html">Footwear</a></li>
+											<li><a href="mens.html">Watches</a></li>
+											<li><a href="mens.html">Accessories</a></li>
+											<li><a href="mens.html">Bags</a></li>
+											<li><a href="mens.html">Caps & Hats</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="mens.html">Jewellery</a></li>
+											<li><a href="mens.html">Sunglasses</a></li>
+											<li><a href="mens.html">Perfumes</a></li>
+											<li><a href="mens.html">Beauty</a></li>
+											<li><a href="mens.html">Shirts</a></li>
+											<li><a href="mens.html">Sunglasses</a></li>
+											<li><a href="mens.html">Swimwear</a></li>
+										</ul>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class="dropdown menu__item">
+						<a href="#" class="dropdown-toggle menu__link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">women's wear <span class="caret"></span></a>
+							<ul class="dropdown-menu multi-column columns-3">
+								<div class="row">
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">Clothing</a></li>
+											<li><a href="womens.html">Wallets</a></li>
+											<li><a href="womens.html">Footwear</a></li>
+											<li><a href="womens.html">Watches</a></li>
+											<li><a href="womens.html">Accessories</a></li>
+											<li><a href="womens.html">Bags</a></li>
+											<li><a href="womens.html">Caps & Hats</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-3 multi-gd-img">
+										<ul class="multi-column-dropdown">
+											<li><a href="womens.html">Jewellery</a></li>
+											<li><a href="womens.html">Sunglasses</a></li>
+											<li><a href="womens.html">Perfumes</a></li>
+											<li><a href="womens.html">Beauty</a></li>
+											<li><a href="womens.html">Shirts</a></li>
+											<li><a href="womens.html">Sunglasses</a></li>
+											<li><a href="womens.html">Swimwear</a></li>
+										</ul>
+									</div>
+									<div class="col-sm-6 multi-gd-img multi-gd-text ">
+										<a href="womens.html"><img src="images/woo.jpg" alt=" "/></a>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</ul>
+					</li>
+					<li class=" menu__item"><a class="menu__link" href="electronics.html">Electronics</a></li>
+					<li class=" menu__item"><a class="menu__link" href="codes.html">Short Codes</a></li>
+					<li class=" menu__item"><a class="menu__link" href="contact.html">contact</a></li>
+				  </ul>
+				</div>
+			  </div>
+			</nav>	
+		</div>
+		<div class="top_nav_right">
+			<div class="cart box_1">
+						<a href="checkout.html">
+							<h3> <div class="total">
+								<i class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></i>
+								<span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</div>
+								
+							</h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+						
+			</div>	
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //banner-top -->
+<!-- banner -->
+<div class="banner-grid">
+	<div id="visual">
+			<div class="slide-visual">
+				<!-- Slide Image Area (1000 x 424) -->
+				<ul class="slide-group">
+					<li><img class="img-responsive" src="images/ba1.jpg" alt="Dummy Image" /></li>
+					<li><img class="img-responsive" src="images/ba2.jpg" alt="Dummy Image" /></li>
+					<li><img class="img-responsive" src="images/ba3.jpg" alt="Dummy Image" /></li>
+				</ul>
+
+				<!-- Slide Description Image Area (316 x 328) -->
+				<div class="script-wrap">
+					<ul class="script-group">
+						<li><div class="inner-script"><img class="img-responsive" src="images/baa1.jpg" alt="Dummy Image" /></div></li>
+						<li><div class="inner-script"><img class="img-responsive" src="images/baa2.jpg" alt="Dummy Image" /></div></li>
+						<li><div class="inner-script"><img class="img-responsive" src="images/baa3.jpg" alt="Dummy Image" /></div></li>
+					</ul>
+					<div class="slide-controller">
+						<a href="#" class="btn-prev"><img src="images/btn_prev.png" alt="Prev Slide" /></a>
+						<a href="#" class="btn-play"><img src="images/btn_play.png" alt="Start Slide" /></a>
+						<a href="#" class="btn-pause"><img src="images/btn_pause.png" alt="Pause Slide" /></a>
+						<a href="#" class="btn-next"><img src="images/btn_next.png" alt="Next Slide" /></a>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	<script type="text/javascript" src="js/pignose.layerslider.js"></script>
+	<script type="text/javascript">
+	//<![CDATA[
+		$(window).load(function() {
+			$('#visual').pignoseLayerSlider({
+				play    : '.btn-play',
+				pause   : '.btn-pause',
+				next    : '.btn-next',
+				prev    : '.btn-prev'
+			});
+		});
+	//]]>
+	</script>
+
+</div>
+<!-- //banner -->
+<!-- content -->
+
+<div class="new_arrivals">
+	<div class="container">
+		<h3><span>new </span>arrivals</h3>
+		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+		<div class="new_grids">
+			<div class="col-md-4 new-gd-left">
+				<img src="images/wed1.jpg" alt=" " />
+				<div class="wed-brand simpleCart_shelfItem">
+					<h4>Wedding Collections</h4>
+					<h5>Flat 50% Discount</h5>
+					<p><i>$250</i> <span class="item_price">$500</span><a class="item_add hvr-outline-out button2" href="#">add to cart </a></p>
+				</div>
+			</div>
+			<div class="col-md-4 new-gd-middle">
+				<div class="new-levis">
+					<div class="mid-img">
+						<img src="images/levis1.png" alt=" " />
+					</div>
+					<div class="mid-text">
+						<h4>up to 40% <span>off</span></h4>
+						<a class="hvr-outline-out button2" href="product.html">Shop now </a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="new-levis">
+					<div class="mid-text">
+						<h4>up to 50% <span>off</span></h4>
+						<a class="hvr-outline-out button2" href="product.html">Shop now </a>
+					</div>
+					<div class="mid-img">
+						<img src="images/dig.jpg" alt=" " />
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="col-md-4 new-gd-left">
+				<img src="images/wed2.jpg" alt=" " />
+				<div class="wed-brandtwo simpleCart_shelfItem">
+					<h4>Spring / Summer</h4>
+					<p>Shop Men</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<!-- //content -->
+
+<!-- content-bottom -->
+
+<div class="content-bottom">
+	<div class="col-md-7 content-lgrid">
+		<div class="col-sm-6 content-img-left text-center">
+			<div class="content-grid-effect slow-zoom vertical">
+				<div class="img-box"><img src="images/p1.jpg" alt="image" class="img-responsive zoom-img"></div>
+					<div class="info-box">
+						<div class="info-content simpleCart_shelfItem">
+									<h4>Mobiles</h4>
+									<span class="separator"></span>
+									<p><span class="item_price">$500</span></p>
+									<span class="separator"></span>
+									<a class="item_add hvr-outline-out button2" href="#">add to cart </a>
+						</div>
+					</div>
+			</div>
+		</div>
+		<div class="col-sm-6 content-img-right">
+			<h3>Special Offers and 50%<span>Discount On</span> Mobiles</h3>
+		</div>
+		
+		<div class="col-sm-6 content-img-right">
+			<h3>Buy 1 get 1  free on <span> Branded</span> Watches</h3>
+		</div>
+		<div class="col-sm-6 content-img-left text-center">
+			<div class="content-grid-effect slow-zoom vertical">
+				<div class="img-box"><img src="images/p2.jpg" alt="image" class="img-responsive zoom-img"></div>
+					<div class="info-box">
+						<div class="info-content simpleCart_shelfItem">
+							<h4>Watches</h4>
+							<span class="separator"></span>
+							<p><span class="item_price">$250</span></p>
+							<span class="separator"></span>
+							<a class="item_add hvr-outline-out button2" href="#">add to cart </a>
+						</div>
+					</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="col-md-5 content-rgrid text-center">
+		<div class="content-grid-effect slow-zoom vertical">
+				<div class="img-box"><img src="images/p4.jpg" alt="image" class="img-responsive zoom-img"></div>
+					<div class="info-box">
+						<div class="info-content simpleCart_shelfItem">
+									<h4>Shoes</h4>
+									<span class="separator"></span>
+									<p><span class="item_price">$150</span></p>
+									<span class="separator"></span>
+									<a class="item_add hvr-outline-out button2" href="#">add to cart </a>
+						</div>
+					</div>
+			</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+<!-- //content-bottom -->
+<!-- product-nav -->
